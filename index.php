52486e687b31acd4840ccb5ad89c4de89a2e7e53
geromuhaya <132812722+geromuhaya@users.noreply.github.com>
2023-06-10 13:59:15 +0100

    Add files via upload
    my first site

---
+++ index.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="box">
+        <span class="borderLine"></span>
+        <form>
+            <h1>sign in</h1>
+            <div class="inputBox">
+                <input type="text" required="required">
+                <span>Usuario</span>
+                <i></i>
+            </div>
+            <div class="inputBox">
+                <input type="password" required="required">
+                <span>Senha</span>
+                <i></i>
+            </div>
+            <div class="links">
+                <a href="#">Esqueceu a senha?</a>
+                <a href="#">Signup</a>
+            </div>
+            <input type="submit" value="Login">
+        </form>
+    </div>
+</body>
+</html>
